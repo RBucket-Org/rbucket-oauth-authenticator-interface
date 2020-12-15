1a4ff363b6df2760b6694974f7fce3a8874a066a
egnimos <egnimos25@gmail.com>
2020-12-15 10:16:22 +0530

    oauth-intialised

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RBucket-Org/rbucket-oauth-authenticator-interface
+
+go 1.15
---
+++ oauth/oauth.go
@@ -0,0 +1,7 @@
+package oauth
+
+/*
+This package contains the authenticator methods where the given access token is get
+ authenticated and the request is passed in the form of the bool value to get the
+auth situation
+*/
